3e5f94b876a8c981147c46731d84a44a30afc13d
SirMorfield <joppe.koers@gmail.com>
2021-08-19 17:25:27 +0200

    module01/ex02

---
+++ module01/ex02/src/main.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <iostream>
+
+int main()
+{
+	std::string *stringPTR = new std::string("HI THIS IS BRAIN");
+	std::string *&stringREF = stringPTR;
+
+	std::cout << "stringPTR address: " << stringPTR << std::endl;
+	std::cout << "stringREF address: " << stringREF << std::endl;
+
+	std::cout << "  stringPTR value: " << *stringPTR << std::endl;
+	std::cout << "  stringREF value: " << *stringREF << std::endl;
+
+	delete stringPTR;
+	return 0;
+}
