3c6025126fb2f474292b06c5ee76809fd26da7e5
SirMorfield <joppe.koers@gmail.com>
2021-09-06 13:36:23 +0200

    copy files

---
+++ module03/ex03/src/FragTrap.cpp
@@ -0,0 +1,32 @@
+#include "FragTrap.hpp"
+
+FragTrap::FragTrap() : ScavTrap("Peanut", 100, 100, 30) {
+	std::cout << "FragTrap " << name << " has joined the realm of the living" << std::endl;
+}
+
+FragTrap::FragTrap(const std::string name) : ScavTrap(name, 100, 100, 30) {
+	std::cout << "FragTrap " << name << " has joined the realm of the living" << std::endl;
+}
+
+FragTrap::FragTrap(const FragTrap& cp) {
+	*this = cp;
+	std::cout << "FragTrap " << name << " has joined the realm of the living" << std::endl;
+}
+
+FragTrap::~FragTrap() {
+	std::cout << "FragTrap " << name << " fell into the void" << std::endl;
+}
+
+FragTrap& FragTrap::operator=(const FragTrap& cp) {
+	hitPoints = cp.hitPoints;
+	energyPoints = cp.energyPoints;
+	attackDamage = cp.attackDamage;
+	initalHitPoints = cp.initalHitPoints;
+	initialEnergyPoints = cp.initialEnergyPoints;
+	initialAttackDamage = cp.initialAttackDamage;
+	return *this;
+}
+
+void FragTrap::highFiveGuys() {
+	std::cout << "FragTrap " << name << " made a positive high five request" << std::endl;
+}
