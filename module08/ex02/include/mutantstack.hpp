285f422a09211d982a472b1cba4d86dc852bc7a9
SirMorfield <joppe.koers@gmail.com>
2021-10-28 12:18:44 +0200

    oops

--- module08/ex02/include/mutantstack.hpp
+++ module08/ex02/include/mutantstack.hpp
@@ -16,12 +16,17 @@ class MutantStack : public std::stack<T> {
 		return *this;
 	};
 
-	typename std::stack<T>::container_type::const_iterator		   begin() const { return (std::stack<T>::c.begin()); }
-	typename std::stack<T>::container_type::iterator			   begin() { return (std::stack<T>::c.begin()); }
-	typename std::stack<T>::container_type::const_iterator		   end() const { return (std::stack<T>::c.end()); }
-	typename std::stack<T>::container_type::iterator			   end() { return (std::stack<T>::c.end()); }
-	typename std::stack<T>::container_type::const_reverse_iterator rbegin() const { return (std::stack<T>::c.rbegin()); }
-	typename std::stack<T>::container_type::reverse_iterator	   rbegin() { return (std::stack<T>::c.rbegin()); }
-	typename std::stack<T>::container_type::const_reverse_iterator rend() const { return (std::stack<T>::c.rend()); }
-	typename std::stack<T>::container_type::reverse_iterator	   rend() { return (std::stack<T>::c.rend()); }
+	typedef typename std::stack<T>::container_type::iterator			   iterator;
+	typedef typename std::stack<T>::container_type::const_iterator		   const_iterator;
+	typedef typename std::stack<T>::container_type::reverse_iterator	   reverse_iterator;
+	typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+	//
+	iterator			   begin() { return (std::stack<T>::c.begin()); }
+	const_iterator		   begin() const { return (std::stack<T>::c.begin()); }
+	iterator			   end() { return (std::stack<T>::c.end()); }
+	const_iterator		   end() const { return (std::stack<T>::c.end()); }
+	reverse_iterator	   rbegin() { return (std::stack<T>::c.rbegin()); }
+	const_reverse_iterator rbegin() const { return (std::stack<T>::c.rbegin()); }
+	reverse_iterator	   rend() { return (std::stack<T>::c.rend()); }
+	const_reverse_iterator rend() const { return (std::stack<T>::c.rend()); }
 };
